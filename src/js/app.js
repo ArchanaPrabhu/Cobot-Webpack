518d65ea53c6932f739f042b93587fa0494e8a01
ArchanaPrabhu <prabhuarc27@gmail.com>
2021-07-29 12:57:01 +0530

    testing

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+import '../scss/app.scss';
+import '../assets/css/nucleo-icons.css';
+import '../assets/css/nucleo-svg.css';
+import '../assets/css/soft-ui-dashboard.css';
+/* Your JS Code goes here */
+
+/* Demo JS */
+import './demo.js';
+import '../assets/js/soft-ui-dashboard.min.js';
+
+/* Images */
+import '../assets/img/favicon.png';
+import '../assets/js/soft-ui-dashboard.min.js';
+import '../assets/img/logo-ct.png';
+import '../assets/img/shapes/waves-white.svg';
